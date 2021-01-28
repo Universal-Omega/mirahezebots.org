f58d474aff1f3cfc3afd117f25ebea856614e1b2
RhinosF1 <rhinosf1@gmail.com>
2021-01-28 14:07:00 +0000

    app: add content generator

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-
+with open('templates/about.html', 'r') as file:
+    contents = file.read()
+    with open('templates/footer.html', 'r') as footer:
+              footerr = footer.read()
+    with open('templates/head.html', 'r') as head:
+              headr = head.read()
+              headr = headr.format(canonical='url', title='Title')
+    contents = contents.format(head=headr, footer=footerr)
+print(contents)
