a16946342afc8069d9fb33e43850bc4f790ca323
RhinosF1 <rhinosf1@gmail.com>
2021-01-29 14:28:11 +0000

    tests: fix

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,10 +1,10 @@
 """Test rendering of HTML by Flask."""
 from MirahezeBots_jsonparser import jsonparser as jp
 
-from .app import display_content
+from app import display_content
 
 
 def test_about_page():
     """Tests about page."""
-    content = display_content('about', jp.createdict('../config.json'))
+    content = display_content('about', jp.createdict('./config.json'))
     assert 'IRC' in content
