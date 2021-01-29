9e137585ab7ed1516b14b8e60732a9503ac55700
RhinosF1 <rhinosf1@gmail.com>
2021-01-29 14:32:36 +0000

    Update test_render.py

--- tests/test_render.py
+++ tests/test_render.py
@@ -7,4 +7,4 @@ from app import display_content
 def test_about_page():
     """Tests about page."""
     content = display_content('about', jp.createdict('./config.json'))
-    assert 'IRC' in content
+    assert 'IRC' in content  # nosec
