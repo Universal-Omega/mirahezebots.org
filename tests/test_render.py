aa8f57d18c489a6599daf498b1f3290ee07b1774
RhinosF1 <rhinosf1@gmail.com>
2021-01-29 14:18:49 +0000

    Update test_render.py

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,6 +1,9 @@
 from .app import display_content
 
+from MirahezeBots_jsonparser import jsonparser as jp
+
 
 def test_about_page():
+    """Tests about page."""
     content = display_content('about', jp.createdict('../config.json'))
     assert 'IRC' in content
