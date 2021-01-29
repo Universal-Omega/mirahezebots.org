a95bd796b45d8fa65ee290cfaace7483a1eed711
RhinosF1 <rhinosf1@gmail.com>
2021-01-29 14:03:29 +0000

    flake8 fixes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, send_file
+"""Powers Flask API for mirahezebots.org web site."""
 from MirahezeBots_jsonparser import jsonparser as jp
+
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 
 def display_content(path, config):
+    """Generates the content for templated pages."""
     with open(config['templatedpages'][path], 'r') as file:
         contents = file.read()
         with open('templates/navbar.html', 'r') as navbar:
@@ -19,7 +22,7 @@ def display_content(path, config):
             if path == 'about':
                 about = 'class="active" href="about"'
             if path == 'documentation':
-                doocs = 'class="active" href="documentation"'
+                docs = 'class="active" href="documentation"'
             if path == 'contribs':
                 contribs = 'class="active" href="contribs"'
             navbarr = navbarr.format(
@@ -28,34 +31,34 @@ def display_content(path, config):
                 contribslink=contribs,
                 aboutlink=about,
                 sourcecodelink=sourcecode,
-                )
+            )
         with open('templates/footer.html', 'r') as footer:
-                  footerr = footer.read()
+            footerr = footer.read()
         with open('templates/head.html', 'r') as head:
-                  headr = head.read()
-                  if path == 'index':
-                      canonical = config['canonical-prefix']  # index is not canonical
-                  else:
-                      canonical = config['canonical-prefix']+path
-                  headr = headr.format(canonical=canonical, title='Title')
-        contents = contents.format(head=headr, footer=footerr, navbar=navbarr)
-    return contents
+            headr = head.read()
+            if path == 'index':
+                canonical = config['canonical-prefix']  # index is not canonical
+            else:
+                canonical = config['canonical-prefix']+path
+            headr = headr.format(canonical=canonical, title='Title')
+    return contents.format(head=headr, footer=footerr, navbar=navbarr)
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    """Generates appropiate response to a request."""
     if path.endswith('.html'):
         path = path[:5]  # ignore .html endings
-    if path == "":
-        path = "index"  # rewrite empty path to index
+    if path == '':
+        path = 'index'  # rewrite empty path to index
     config = jp.createdict('config.json')
     if path in config['directshow']:
         return send_file(config['directshow'][path])
     if path in config['templatedpages']:
         return display_content(path, config)
-    
+    return Response(404)
+  
 
 if __name__ == '__main__':
     app.run(debug=True)
-
