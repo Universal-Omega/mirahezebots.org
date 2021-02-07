e750d2cde27da4540fb39eda3872b92def89341f
RhinosF1 <rhinosf1@gmail.com>
2021-02-07 15:05:58 +0000

    Update test_render.py

--- tests/test_render.py
+++ tests/test_render.py
@@ -4,7 +4,14 @@ from MirahezeBots_jsonparser import jsonparser as jp
 from app import display_content
 
 
+XSS = {"templatedpages":{"about":"templates/about.html","contribs":"templates/contribs.html","documentation":"templates/documentation.html","index":"templates/index.html","privacy":"templates/privacy.html","terms":"templates/terms.html"},"directshow":{"robots.txt":"templates/robots.txt","sitemap.xml":"templates/sitemap.xml","css/common.css":"assets/common.css","logo.png":"assets/logo.png","favicon.png":"assets/favicon.png","fosshost.png":"assets/fosshost.png"},"title":{"index":"</title><img src=1 href=1 onerror=\"javascript:alert(1)\"></img>","about":"About - MirahezeBot","documentation":"Documentation - MirahezeBot","contribs":"Contributions - MirahezeBot","privacy":"Privacy - MirahezeBot","terms":"Terms - MirahezeBot"},"font-awesome-url":"Font-Awesome","Font-awesome-path":"Font-Awesome","canonical-prefix":"https://bots.miraheze.wiki/","rendertemplates":{"navbar":"templates/navbar.html","footer":"templates/footer.html","head":"templates/head.html"}}
+
 def test_about_page():
     """Tests about page."""
     content = display_content('about', jp.createdict('./config.json'))
     assert 'IRC' in content  # nosec
+
+def test_title_xss():
+    content = display_content('index', XSS)
+    assert '<title></title><img src=1 href=1 onerror="javascript:alert(1)"></img></title>' not in content
+    assert '<title>&lt;/title&gt;&lt;img src=1 href=1 onerror=&#34;javascript:alert(1)&#34;&gt;&lt;/img&gt;</title>' in content
